d835b96981459baf0619b269977cfc983835f55e
[suyash patel] <suyashpatelmrj@gmail.com>
2021-12-28 10:38:25 +0530

    Binarytostring

--- PrateekBHaiya/3CharacterArrays/strings/9BinaryStringToNumber.cpp
+++ PrateekBHaiya/3CharacterArrays/strings/9BinaryStringToNumber.cpp
@@ -1,6 +1,36 @@
+/*
+
+Expected Time Complexity : O(N)
+
+
 #include <iostream>
 #include <string>
+using namespace std;
 
+// Function to convert binary to decimal
+int binaryToDecimal(string n)
+{
+    string num = n;
+    int dec_value = 0;
+
+    // Initializing base value to 1, i.e 2^0
+    int base = 1;
+
+    int len = num.length();
+    for (int i = len - 1; i >= 0; i--) {
+        if (num[i] == '1')
+            dec_value += base;
+        base = base * 2;
+    }
+
+    return dec_value;
+}
+
+*/
+
+#include <iostream>
+#include <string>
+#include <math.h>
 using namespace std;
 int binaryToDecimal(string s)
 {
@@ -8,8 +38,7 @@ int binaryToDecimal(string s)
     int power = 0;
     for (int i = s.size() - 1; i >= 0; i--)
     {
-        // int temp = s[i] - '0';
-        num = num + ((s[i] - '0') * (2 ^ power));
+        num = num + ((s[i] - '0') * pow(2, power));
         power++;
     }
     return num;
