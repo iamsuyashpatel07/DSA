bae3ba00a7e558e7e679045de650b68981f7f98c
[suyash patel] <suyashpatelmrj@gmail.com>
2022-01-05 22:10:44 +0530

    odd even

---
+++ PrateekBHaiya/6-BitManipulation/findoddeven.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    if (a & 1)
+    {
+        cout << "odd";
+    }
+    else
+    {
+        cout << "even";
+    }
+    return 0;
+}
