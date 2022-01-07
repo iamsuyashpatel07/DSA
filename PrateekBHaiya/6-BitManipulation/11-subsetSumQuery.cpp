7dd7330905e842277d4c891ffc3de765ec3ec9d7
[suyash patel] <suyashpatelmrj@gmail.com>
2022-01-07 16:33:25 +0530

    subsetsum

---
+++ PrateekBHaiya/6-BitManipulation/11-subsetSumQuery.cpp
@@ -0,0 +1,36 @@
+// Expected Time Complexity : O(1) for each query
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<bool> subsetSum(vector<int> v, vector<int> q)
+{
+    int n = q.size();
+    vector<bool> b(n);
+
+    bitset<10000> bit;
+    bit.reset();
+    bit[0] = 1;
+
+    for (int i = 0; i < v.size(); ++i)
+        bit |= (bit << v[i]);
+
+    for (int i = 0; i < n; i++)
+    {
+        int x = q[i];
+        bit[x] ? b[i] = true : b[i] = false;
+    }
+
+    return b;
+}
+int main()
+{
+    vector<int> num = {1, 2, 3};
+    vector<int> query = {5, 3, 8};
+    vector<bool> res = subsetSum(num, query);
+    for (auto x : res)
+    {
+        cout << x << " ";
+    }
+    return 0;
+}
