d1134fd0d7fb2561a1f14a0de1475db3e06d83a9
[suyash patel] <suyashpatelmrj@gmail.com>
2022-01-10 13:15:03 +0530

    occurence

--- PrateekBHaiya/7-Recursion/5-Occurence.cpp
+++ PrateekBHaiya/7-Recursion/5-Occurence.cpp
@@ -20,11 +20,40 @@ int firstOcc(int arr[], int size, int key)
 
     return -1;
 }
+int lastOcc(int arr[], int size, int key)
+{
+    // base case
+    if (size == 0)
+    {
+        return -1;
+    }
+    // recursive case
+    int ans = lastOcc(arr + 1, size - 1, key);
+    if (ans == -1)
+    {
+        if (arr[0] == key)
+        {
+            return 0;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    else
+    {
+        return ans + 1;
+    }
+
+    return -1;
+}
 int main()
 {
     int arr[] = {1, 2, 3, 4, 5, 6, 6, 8, 7};
     int size = sizeof(arr) / sizeof(arr[0]);
-    int key = 8;
+    int key = 6;
     cout << firstOcc(arr, size, key);
+    cout << endl
+         << lastOcc(arr, size, key);
     return 0;
 }
