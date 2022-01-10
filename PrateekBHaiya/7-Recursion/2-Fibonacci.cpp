2cade3fdb162d98cd0765321fa81f7c6a047a51f
[suyash patel] <suyashpatelmrj@gmail.com>
2022-01-10 10:34:06 +0530

    FIBO

--- PrateekBHaiya/7-Recursion/2-Fibonacci.cpp
+++ PrateekBHaiya/7-Recursion/2-Fibonacci.cpp
@@ -1,13 +1,16 @@
+// TIME COMPLEXITY O(2^N)
+// SPACE COMPLEXITY O(N)
+
 #include <bits/stdc++.h>
 using namespace std;
 int fibo(int i)
 {
-    if (i == 0 or i == 1)
+    if (i == 0 or i == 1) // base case
     {
         return i;
     }
 
-    return fibo(i - 1) + fibo(i - 2);
+    return fibo(i - 1) + fibo(i - 2); // recursive case
 }
 int main()
 {
