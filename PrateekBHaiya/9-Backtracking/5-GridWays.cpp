dbccaf35d2c805540fcacdff80dc117886bbbcb5
[suyash patel] <suyashpatelmrj@gmail.com>
2022-02-19 13:19:29 +0530

    gridpathways

---
+++ PrateekBHaiya/9-Backtracking/5-GridWays.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int gridways(int i, int j, int m, int n)
+{
+    // base case
+    if (i == m - 1 and i == n - 1)
+    {
+        return 1;
+    }
+    // edge case
+    if (i >= m or j >= n)
+    {
+        return 0;
+    }
+    // recursive case
+    int ans = gridways(i + 1, j, m, n) + gridways(i, j + 1, m, n);
+    return ans;
+}
+int main()
+{
+    int m, n;
+    cin >> m >> n;
+    cout << "number of path " << gridways(0, 0, m, n);
+    return 0;
+}
