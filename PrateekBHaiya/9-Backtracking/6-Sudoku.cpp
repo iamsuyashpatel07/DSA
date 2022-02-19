ea9d22254786ff3fce1dd222e3ebc274d575b582
[suyash patel] <suyashpatelmrj@gmail.com>
2022-02-19 16:05:16 +0530

    suudoku

--- PrateekBHaiya/9-Backtracking/6-Sudoku.cpp
+++ PrateekBHaiya/9-Backtracking/6-Sudoku.cpp
@@ -1,11 +1,14 @@
+// formula for subgrid starting and ending of that grid
+//(sx,sy)=(3,0)
+// sx=(x/3)*3=(4/3)*3=3
+// sy=(y/3)*3=(1/3)*3=0
+
 #include <iostream>
 #include <cmath>
 using namespace std;
-
 bool isSafe(int mat[][9], int i, int j, int no)
 {
-
-    // Check for row and col
+    // check for row and col
     for (int k = 0; k < 9; k++)
     {
         if (mat[k][j] == no || mat[i][k] == no)
@@ -14,10 +17,8 @@ bool isSafe(int mat[][9], int i, int j, int no)
         }
     }
     // check for subgrid
-
     int sx = (i / 3) * 3;
     int sy = (j / 3) * 3;
-
     for (int x = sx; x < sx + 3; x++)
     {
         for (int y = sy; y < sy + 3; y++)
@@ -30,7 +31,6 @@ bool isSafe(int mat[][9], int i, int j, int no)
     }
     return true;
 }
-
 bool solveSudoku(int mat[][9], int i, int j, int n)
 {
     // base case
@@ -48,27 +48,23 @@ bool solveSudoku(int mat[][9], int i, int j, int n)
 
         return true;
     }
-
     // rec case
     if (j == n)
     {
         return solveSudoku(mat, i + 1, 0, n);
     }
-
-    // skip  the prefilled cell
+    // skip the prefilled cell
     if (mat[i][j] != 0)
     {
         return solveSudoku(mat, i, j + 1, n);
     }
-
     // cell to be filled
-    // try out all possibilites
+    // try out all possibilities
     for (int no = 1; no <= n; no++)
     {
-        // whether it is safe to place the number or not
+        // whether it is safe  place the number or not
         if (isSafe(mat, i, j, no))
         {
-
             mat[i][j] = no;
             bool solveSubproblem = solveSudoku(mat, i, j + 1, n);
             if (solveSubproblem == true)
@@ -77,27 +73,26 @@ bool solveSudoku(int mat[][9], int i, int j, int n)
             }
         }
     }
-    // if no option works, backtracking
+    // if no option work
     mat[i][j] = 0;
     return false;
 }
-
 int main()
 {
     int n = 9;
     int mat[9][9] =
-        {{5, 3, 0, 0, 7, 0, 0, 0, 0},
-         {6, 0, 0, 1, 9, 5, 0, 0, 0},
-         {0, 9, 8, 0, 0, 0, 0, 6, 0},
-         {8, 0, 0, 0, 6, 0, 0, 0, 3},
-         {4, 0, 0, 8, 0, 3, 0, 0, 1},
-         {7, 0, 0, 0, 2, 0, 0, 0, 6},
-         {0, 6, 0, 0, 0, 0, 2, 8, 0},
-         {0, 0, 0, 4, 1, 9, 0, 0, 5},
-         {0, 0, 0, 0, 8, 0, 0, 7, 9}};
-
+        {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}};
     if (!solveSudoku(mat, 0, 0, n))
     {
-        cout << "No solution exists!";
+        cout << "NO solution exists!";
     }
 }
