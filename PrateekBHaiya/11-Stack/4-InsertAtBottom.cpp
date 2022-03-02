41900f20d60902458064a01bb33c49256e6baf2e
[suyash patel] <suyashpatelmrj@gmail.com>
2022-03-02 13:28:06 +0530

    InsertAtBottom

---
+++ PrateekBHaiya/11-Stack/4-InsertAtBottom.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+// Challenge : Insert at Stack Bottom
+void insertAtBottom(stack<int> &s, int data)
+{
+    // base case
+    if (s.empty())
+    {
+        s.push(data);
+        return;
+    }
+    // rec case
+    int temp = s.top();
+    s.pop();
+
+    insertAtBottom(s, data);
+    s.push(temp);
+}
+
+int main()
+{
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+
+    insertAtBottom(s, 5);
+
+    while (!s.empty())
+    {
+        cout << s.top() << endl;
+        s.pop();
+    }
+
+    return 0;
+}
