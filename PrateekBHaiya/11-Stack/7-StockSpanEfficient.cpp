efd0b50afc51a3a508366656023c0281b31ac921
[suyash patel] <suyashpatelmrj@gmail.com>
2022-03-02 21:56:27 +0530

    Effficient

---
+++ PrateekBHaiya/11-Stack/7-StockSpanEfficient.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+// Stack Span Problem
+
+void stockSpan(int prices[], int n, int span[])
+{
+    stack<int> s; // indices of the days
+    s.push(0);
+    span[0] = 1;
+    // loop for rest of the days
+    for (int i = 1; i <= n - 1; i++)
+    {
+        int currentPrice = prices[i];
+        // top most element that is higher then current prices
+        while (!s.empty() and prices[s.top()] <= currentPrice)
+        {
+            s.pop();
+        }
+        if (!s.empty())
+        {
+            int prevHighest = s.top();
+            span[i] = i - prevHighest;
+        }
+        else
+        {
+            span[i] = i + 1;
+        }
+        // push this element into the stack
+        s.push(i);
+    }
+}
+
+int main()
+{
+    int prices[] = {100, 80, 60, 70, 60, 75, 85};
+    int n = sizeof(prices) / sizeof(int);
+    int span[100000] = {0};
+    stockSpan(prices, n, span);
+    for (int i = 0; i < n; i++)
+    {
+        cout << span[i] << " ";
+    }
+
+    return 0;
+}
