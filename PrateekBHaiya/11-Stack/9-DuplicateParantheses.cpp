b38ddbb3c427110429a901cd46f93b8b792651bc
[suyash patel] <suyashpatelmrj@gmail.com>
2022-03-05 21:35:31 +0530

    duplicateParentheses

---
+++ PrateekBHaiya/11-Stack/9-DuplicateParantheses.cpp
@@ -0,0 +1,44 @@
+
+// Expected Time Complexity: O(n)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool duplicateParentheses(string str)
+{
+
+    stack<char> Stack;
+
+    for (char ch : str)
+    {
+        if (ch == ')')
+        {
+            char top = Stack.top();
+            Stack.pop();
+
+            int elementsInside = 0;
+            while (top != '(')
+            {
+                elementsInside++;
+                top = Stack.top();
+                Stack.pop();
+            }
+            if (elementsInside < 1)
+            {
+                return true;
+            }
+        }
+
+        else
+            Stack.push(ch);
+    }
+
+    return false;
+}
+int main()
+{
+    string str;
+    getline(cin, str);
+    cout << duplicateParentheses(str);
+    return 0;
+}
