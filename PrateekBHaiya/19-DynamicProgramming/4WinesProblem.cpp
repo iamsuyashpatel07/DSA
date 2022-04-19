bce6933ac9e73d031861f1cd014a25c82ef03577
[suyash patel] <suyashpatelmrj@gmail.com>
2022-04-19 21:00:26 +0530

    Wines Problem

--- PrateekBHaiya/19-DynamicProgramming/4WinesProblem.cpp
+++ PrateekBHaiya/19-DynamicProgramming/4WinesProblem.cpp
@@ -21,6 +21,7 @@ Explanation
 2*1+4*2+1*3+3*4+5*5=50
 */
 #include <iostream>
+#include <vector>
 using namespace std;
 // top down dp code
 int wines(int dp[][10], int prices[], int L, int R, int y)
@@ -41,12 +42,45 @@ int wines(int dp[][10], int prices[], int L, int R, int y)
     int pick_right = y * prices[R] + wines(dp, prices, L, R - 1, y + 1);
     return dp[L][R] = max(pick_right, pick_left);
 }
+// Bottom Up Approach
+int wines_bottom_up(int prices[], int n)
+{
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                dp[i][i] = n * prices[i];
+            }
+            else if (i <= j)
+            {
+                // dp(i...j)
+                int y = n - (j - i);
+                int pick_left = prices[i] * y + dp[i + 1][j];
+                int pick_right = prices[j] * y + dp[i][j - 1];
+                dp[i][j] = max(pick_left, pick_right);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << dp[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return dp[0][n - 1];
+}
 
 int main()
 {
     int a[] = {2, 3, 5, 1, 4};
     int n = 5;
     int dp[10][10] = {0};
+    cout << "Top Down Approach" << endl;
     cout << wines(dp, a, 0, n - 1, 1) << endl;
     for (int i = 0; i < n; i++)
     {
@@ -56,6 +90,7 @@ int main()
         }
         cout << endl;
     }
-
+    cout << "Bottom Up Approach" << endl;
+    cout << wines_bottom_up(a, n) << endl;
     return 0;
 }
