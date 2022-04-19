7ceb68942099a8268b67f3163d986c497bf2d53c
[suyash patel] <suyashpatelmrj@gmail.com>
2022-04-19 14:18:20 +0530

    wines

---
+++ PrateekBHaiya/19-DynamicProgramming/4WinesProblem.cpp
@@ -0,0 +1,15 @@
+/*
+There is a collection of N wines placed linearly on shelf,
+the price of the ith wine is pi.
+Since wines get better every year,suppose today is the
+year 1,on the year y the priceof the ith wine be y*pi i.e.
+y-times the value that current year.
+You want to sell all the wines you have,but you want to
+sell exactly one wine per year,starthing this year.On
+more constraint - on each year you cal sell either the
+leftmost or the rightmost wine without re-ordering.
+Find the maximum profit you can get if you sell all the
+wines in the optimal order.
+
+
+*/
