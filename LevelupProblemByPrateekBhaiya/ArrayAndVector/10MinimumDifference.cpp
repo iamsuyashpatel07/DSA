62e49275e3fb732164ad458af48c20e71ef2aac2
[suyash patel] <suyashpatelmrj@gmail.com>
2022-05-15 12:36:42 +0530

    minimum difference

---
+++ LevelupProblemByPrateekBhaiya/ArrayAndVector/10MinimumDifference.cpp
@@ -0,0 +1,31 @@
+/*
+O(N) :
+https://github.com/coding-minutes/dsa-levelUp-solutions-cpp/blob/master/Section%2002%20-%20array%20and%20vectors/minimumDifference.cpp
+*/
+// O(N^2)
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+pair<int, int> minDifference(vector<int> a, vector<int> b)
+{
+    // Complete this method
+    pair<int, int> result;
+    int m = INT_MAX;
+    for (auto x : a)
+    {
+        for (auto z : b)
+        {
+            int t = abs(x - z);
+            if (t < m)
+            {
+                m = t;
+                result.first = x;
+                result.second = z;
+            }
+        }
+    }
+
+    return result;
+}
