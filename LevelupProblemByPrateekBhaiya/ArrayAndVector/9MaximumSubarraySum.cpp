98b5a85ccc702a11cc3806432e6fa071a3ba08bb
[suyash patel] <suyashpatelmrj@gmail.com>
2022-05-15 12:13:31 +0530

    Max subarray

---
+++ LevelupProblemByPrateekBhaiya/ArrayAndVector/9MaximumSubarraySum.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int maxSubarraySum(vector<int> arr)
+{
+    // Complete this function, your function should return the maximum subarray sum
+    int n = arr.size();
+    int check = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > 0)
+        {
+            check = 1;
+        }
+    }
+    if (check == 0)
+    {
+        return check;
+    }
+    else
+    {
+        int max_so_far = INT_MIN, max_ending_here = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            max_ending_here = max_ending_here + arr[i];
+            if (max_so_far < max_ending_here)
+                max_so_far = max_ending_here;
+
+            if (max_ending_here < 0)
+                max_ending_here = 0;
+        }
+        return max_so_far;
+    }
+}
